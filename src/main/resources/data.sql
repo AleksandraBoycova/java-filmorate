8c0091c4c8b15b9bd66f1db1344453234523945a
Александра <rok@bk.ru>
2023-02-04 12:55:04 +0300

    ТЗ11

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,6 @@ INSERT INTO genre(genre_name) VALUES ('Комедия');
 
 INSERT INTO MPA(mpa_name, description) VALUES ('G', 'у фильма нет возрастных ограничений');
     	INSERT INTO MPA(mpa_name, description) VALUES ('PG', 'детям рекомендуется смотреть фильм с родителями');
-    	INSERT INTO MPA(mpa_name, description) VALUES ('PG_13', 'детям до 13 лет просмотр не желателен');
+    	INSERT INTO MPA(mpa_name, description) VALUES ('PG-13', 'детям до 13 лет просмотр не желателен');
     	INSERT INTO MPA(mpa_name, description) VALUES ('R', 'лицам до 17 лет просматривать фильм можно только в присутствии взрослого');
-    	INSERT INTO MPA(mpa_name, description) VALUES ('NC_17', 'лицам до 18 лет просмотр запрещён');
+    	INSERT INTO MPA(mpa_name, description) VALUES ('NC-17', 'лицам до 18 лет просмотр запрещён');
